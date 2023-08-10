b698cf5bb14230d9920e29d81ddab747399cd6d9
BuketSenol <124041510+BuketSenol@users.noreply.github.com>
2023-08-10 15:46:45 +0300

    url include kaldırıdı normal yazıldı

--- stajProjesi/urls.py
+++ stajProjesi/urls.py
@@ -1,13 +1,20 @@
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from appUser.views import *
+from appMy.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('appMy.urls')),
-    path('',include('appUser.urls'))
+    # appMY
+    path('',dashboardPage, name='dashboardPage'),
+    path('forumDetail',forumDetail, name='forumDetail'),
+    
+    # appUser
+    path('loginPage', loginPage , name='loginPage'),
+    path('postDetail',postDetail, name='postDetail'),
     
 ]+ static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
