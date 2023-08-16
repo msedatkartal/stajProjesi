2e750a64fe7fae856af2e26ad19ff5f0931623be
BuketSenol <124041510+BuketSenol@users.noreply.github.com>
2023-08-16 23:59:06 +0300

    .

--- appUser/models.py
+++ appUser/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from appMy.models import GameCard
+from django.utils.text import slugify
 
 # User Model
 
@@ -10,7 +11,11 @@ class Comment(models.Model):
     date_now =models.DateTimeField(("Tarih - Saat"),auto_now_add = True)
     subject_brand=models.CharField(("Konu Başlığı"), max_length=100, null=True)
     game_cate= models.ForeignKey(GameCard, verbose_name=("Konuya bağlı Kategori İsmi"), on_delete=models.CASCADE, null=True)
-   
+    slug=models.SlugField(blank=True, null=True)
+
+    def save(self,*args, **kwargs):
+        self.slug=slugify(self.subject_brand.replace("ı","i"))
+        super(Comment,self).save(*args, **kwargs)
 
     def __str__(self):
         return self.subject_brand
